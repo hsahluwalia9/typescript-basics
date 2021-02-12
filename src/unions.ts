ef0b456be79febd7d8c5a76170427e94d0ad635b
Harsimar Ahluwalia <harsimar.ahluwalia@gmail.com>
2021-02-11 20:07:48 -0500

    Added unions plus other ts config

---
+++ src/unions.ts
@@ -0,0 +1,7 @@
+let confusedNumber: number | string = 3;
+
+confusedNumber = "3";
+console.log(`${confusedNumber} is a type of ${typeof(confusedNumber)}`);
+
+confusedNumber = 4;
+console.log(`${confusedNumber} is a type of ${typeof(confusedNumber)}`);
